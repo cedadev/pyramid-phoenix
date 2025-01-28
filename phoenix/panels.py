79bfd41c62dbef6125f883de11958a7493625281
Adrian Debski <adriandebski14@gmail.com>
2025-01-28 14:17:45 +0000

    test2

--- phoenix/panels.py
+++ phoenix/panels.py
@@ -37,6 +37,7 @@ def messages(context, request):
         request                    
     )
 
+
 @panel_config(name='breadcrumbs')
 def breadcrumbs(context, request):
     lm = request.layout_manager
